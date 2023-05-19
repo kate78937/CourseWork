cc2b660dc5cab0a23ebf7f35a53d07a660f1b364
kate78937 <kate78937k@gmail.com>
2023-05-19 17:11:09 +0300

    models

--- src/models/models.ts
+++ src/models/models.ts
@@ -1,1 +1,50 @@
 /** @format */
+
+import DataBase from "../database/database";
+import DataTypes from "sequelize";
+
+const User = DataBase.define("user", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  email: { type: DataTypes.STRING, unique: true },
+  password: { type: DataTypes.STRING },
+  role: { type: DataTypes.STRING, defaultValue: "USER" },
+  fullname: { type: DataTypes.STRING },
+  DateofBirth: { type: DataTypes.STRING },
+  mail: { type: DataTypes.STRING },
+  interests: { type: DataTypes.STRING },
+});
+
+const Basket = DataBase.define("basket", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+const Messages = DataBase.define("messages", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  messages: { type: DataTypes.STRING },
+});
+
+const Preferences = DataBase.define("preferences", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  Agefrom: { type: DataTypes.STRING },
+  Ageupto: { type: DataTypes.STRING },
+  mail: { type: DataTypes.STRING },
+  Distance: { type: DataTypes.FLOAT },
+});
+
+const Likes = DataBase.define("likes", {
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+});
+
+User.hasOne(Basket);
+Basket.belongsTo(User);
+
+User.hasMany(Messages);
+Messages.belongsTo(User);
+
+User.hasMany(Preferences);
+Preferences.belongsTo(User);
+
+User.hasMany(Likes);
+Likes.belongsTo(User);
+
+export { User, Basket, Messages, Preferences, Likes };
