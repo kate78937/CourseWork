9f43b4b40522c151baff5926896f2cd4b34abe54
kate78937 <kate78937k@gmail.com>
2023-05-19 17:15:51 +0300

    user router

--- src/routes/routers.ts
+++ src/routes/routers.ts
@@ -1,1 +1,14 @@
 /** @format */
+
+import { Router } from "express";
+import UserController from "../controllers/userController";
+import auth from "../middleware/authMiddleware";
+
+const router = Router();
+
+router.post("/registration", UserController.registration);
+router.post("/login", UserController.login);
+router.put("/userupdate", auth, UserController.update);
+router.get("/check", auth, UserController.check);
+
+export default router;
